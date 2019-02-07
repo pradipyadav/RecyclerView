0eda07bb5505b3347ca983d8429fe0dbb3e64d47
sony <yadav.pradip1995@gmail.com>
2019-02-07 23:08:37 +0530

    New Recycler

---
+++ recyclerproductcart/src/main/java/sony/com/recyclerproductcart/Product.java
@@ -0,0 +1,56 @@
+package sony.com.recyclerproductcart;
+
+public class Product {
+
+    private String name;
+    private String realname;
+    private String team;
+    private int firstappearance;
+    private String createdby;
+    private String publisher;
+    private String imageurl;
+    private String bio;
+
+    public Product(String name, String realname, String team, int firstappearance, String createdby, String publisher, String imageurl, String bio) {
+        this.name = name;
+        this.realname = realname;
+        this.team = team;
+        this.firstappearance = firstappearance;
+        this.createdby = createdby;
+        this.publisher = publisher;
+        this.imageurl = imageurl;
+        this.bio = bio;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getRealname() {
+        return realname;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public int getFirstappearance() {
+        return firstappearance;
+    }
+
+    public String getCreatedby() {
+        return createdby;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public String getImageurl() {
+        return imageurl;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+}
---
+++ recyclerproductcart/src/main/java/sony/com/recyclerproductcart/ProductAdapter.java
@@ -0,0 +1,71 @@
+package sony.com.recyclerproductcart;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductViewHolder> {
+
+    private Context mCtx;
+    private List<Product> productList;
+
+    public ProductAdapter(Context mCtx, List<Product> productList) {
+        this.mCtx = mCtx;
+        this.productList = productList;
+    }
+
+    @NonNull
+    @Override
+    public ProductViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        LayoutInflater inflater = LayoutInflater.from(mCtx);
+        View view = inflater.inflate(R.layout.product_list, null);
+        return new ProductViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ProductViewHolder productViewHolder, int i) {
+
+        Product product = productList.get(i);
+
+        //loading the image
+        Glide.with(mCtx)
+                .load(product.getImageurl())
+                .into(productViewHolder.imageView);
+
+        productViewHolder.textViewTitle.setText(product.getName());
+        productViewHolder.textViewShortDesc.setText(product.getRealname());
+        productViewHolder.textViewRating.setText(product.getTeam());
+        productViewHolder.textViewPrice.setText(String.valueOf(product.getFirstappearance()));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return productList.size();
+    }
+
+    class ProductViewHolder extends RecyclerView.ViewHolder {
+
+        TextView textViewTitle, textViewShortDesc, textViewRating, textViewPrice;
+        ImageView imageView;
+
+        public ProductViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            textViewTitle = itemView.findViewById(R.id.textViewTitle);
+            textViewShortDesc = itemView.findViewById(R.id.textViewShortDesc);
+            textViewRating = itemView.findViewById(R.id.textViewRating);
+            textViewPrice = itemView.findViewById(R.id.textViewPrice);
+            imageView = itemView.findViewById(R.id.imageView);
+        }
+    }
+}
